fe81d0d73d77b314f0c3d6cf3d7e228f73495241
Welingson Expedito dos Santos <welingson12@gmail.com>
2025-03-13 21:43:50 -0300

    refactor: rename table

--- app/Http/Requests/Report/StoreReportImageRequest.php
+++ app/Http/Requests/Report/StoreReportImageRequest.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace App\Http\Requests\Post;
+namespace App\Http\Requests\Report;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StorePostRequest extends FormRequest
+class StoreReportImageRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
     /**
@@ -22,9 +22,7 @@ class StorePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|string|max:255',
-            'content' => 'required|string',
-            'category_id' => 'required|exists:categories,id',
+            //
         ];
     }
 }
